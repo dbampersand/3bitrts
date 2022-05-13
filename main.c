337fb45435a9816b948d1369609c4085da9a5ff2
aaa <thepinkampersand@gmail.com>
2022-05-13 13:16:35 -0400

    linux builds

--- main.c
+++ main.c
@@ -893,6 +893,7 @@ int main(int argc, char* args[])
     al_init_font_addon();
     al_init_ttf_addon();
     al_init_image_addon();
+    al_init_primitives_addon();
     al_install_mouse();
     al_install_keyboard();
     init_lua();
